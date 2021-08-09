a8b8b35c2b346939e5eb1722c927ca632d97cc85
maurilioSousa-Desing <fmaurilio6@gmail.com>
2021-08-09 19:06:48 -0300

    Create PersonMapper.java

---
+++ src/main/java/com/api/AgendaApi/mapper/PersonMapper.java
@@ -0,0 +1,21 @@
+package com.api.AgendaApi.mapper;
+
+import com.api.AgendaApi.dto.request.PersonDTO;
+import com.api.AgendaApi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper
+    public interface PersonMapper {
+
+        PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+        @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+        Person toModel(PersonDTO personDTO);
+
+        PersonDTO toDTO(Person person);
+    }
+
+
