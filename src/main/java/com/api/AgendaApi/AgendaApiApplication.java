ef2c140d2d90a43f202930b8c349dc1cc700ae4c
maurilioSousa-Desing <fmaurilio6@gmail.com>
2021-08-09 19:08:17 -0300

    Create AgendaApiApplication.java

---
+++ src/main/java/com/api/AgendaApi/AgendaApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.AgendaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaApiApplication.class, args);
+	}
+
+}
